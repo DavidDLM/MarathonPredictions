1561a805c73a40d60e6c6d4dc9aa65c6602c474d
mddeleon <mariodeleonm1@gmail.com>
2024-11-19 08:25:37 -0600

    added main

--- app.py
+++ app.py
@@ -37,3 +37,6 @@ def updateData():
     if not data:
         return jsonify({"error": "No data provided"}), 400
     return jsonify({"message": "Data received successfully", "data": data}), 200
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=5000)
